22fda2905556df3897cb7d9b593f0a72d124e118
Fede <fede.delisi@gmail.com>
2025-12-08 16:36:15 +0800

    Add basic dashboard stats

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header.tsx'
+import DashboardStats from './components/DashboardStats.tsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header />
+      <div className='space-y-4'>
+        <Header />
+        <DashboardStats />
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
