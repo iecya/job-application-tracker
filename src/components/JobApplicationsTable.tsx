db6e789577f7286b9f3ddad2133b54c5739cbbfa
Fede <fede.delisi@gmail.com>
2025-12-09 14:46:36 +0800

    Add new application button

--- src/components/JobApplicationsTable.tsx
+++ src/components/JobApplicationsTable.tsx
@@ -9,53 +9,49 @@ function JobApplicationsTable() {
 
     return(
         <>
-            <h2 className='text-2xl font-bold text-center'>Job Applications</h2>
-            <div className='space-y-15'>
-                {mockJobs.map(job => (
-                    <table className='w-full text-sm table-fixed text-left border-collapse bg-teal-950/10 lg:hidden'>
-                        <thead className='bg-teal-900/50 text-3xl text-center font-bold'>
-                            <tr>
-                                <th colSpan={2} className='p-3'>{job.title}</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {Object.entries(job).filter(([k, _]) => k !== "id" && k !== "title").map(([dataKey, dataValue]) => (
+            <div className='space-y-4'>
+                <h2 className='text-2xl font-bold text-center'>Job Applications</h2>
+                <button className='bg-teal-800 py-2 px-3 w-auto mx-auto block font-semibold hover:bg-teal-900 hover:border-transparent focus:outline-teal-600 focus-visible:outline-teal-600'>New Application</button>
+                <div className='space-y-15'>
+                    {mockJobs.map(job => (
+                        <table className='w-full text-sm table-fixed text-left border-collapse bg-teal-950/10 lg:hidden'>
+                            <thead className='bg-teal-900/50 text-3xl text-center font-bold'>
                                 <tr>
-                                    <td className='w-1/3 border-y border-gray-600 px-2 py-3 font-bold'>{formatKeyLabel(dataKey)}</td>
-                                    <td className='w-2/3 border-y border-gray-600 px-2 py-3'>{formatValue(dataKey, dataValue)}</td>
+                                    <th colSpan={2} className='p-3'>{job.title}</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                ))}
-
-
-                <div className='w-full overflow-x-auto'>
-                    <table className='table-auto min-w-max text-md text-left border-collapse bg-teal-950/10 hidden lg:block'>
-                        <thead className='bg-teal-900/50 font-bold'>
-                            <tr>
-                                {headers.map(h => (
-                                    <th className='p-6'>{formatKeyLabel(h)}</th>
-
+                            </thead>
+                            <tbody>
+                                {Object.entries(job).filter(([k, _]) => k !== "id" && k !== "title").map(([dataKey, dataValue]) => (
+                                    <tr>
+                                        <td className='w-1/3 border-y border-gray-600 px-2 py-3 font-bold'>{formatKeyLabel(dataKey)}</td>
+                                        <td className='w-2/3 border-y border-gray-600 px-2 py-3'>{formatValue(dataKey, dataValue)}</td>
+                                    </tr>
                                 ))}
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {mockJobs.map(job => (
-                                <tr className='hover:bg-teal-800/10'>
-                                    {headers.map(header => (
-                                        <td className='border-y border-gray-600 p-6 max-w-lg'>{formatValue(header, job[header] ?? "")}</td>
+                            </tbody>
+                        </table>
+                    ))}
+
+                    <div className='w-full overflow-x-auto'>
+                        <table className='table-auto min-w-max text-md text-left border-collapse bg-teal-950/10 hidden lg:block'>
+                            <thead className='bg-teal-900/50 font-bold'>
+                                <tr>
+                                    {headers.map(h => (
+                                        <th className='p-6'>{formatKeyLabel(h)}</th>
                                     ))}
                                 </tr>
-                                
-                            ))}
-                            
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                {mockJobs.map(job => (
+                                    <tr className='hover:bg-teal-800/10'>
+                                        {headers.map(header => (
+                                            <td className='border-y border-gray-600 p-6 max-w-lg'>{formatValue(header, job[header] ?? "")}</td>
+                                        ))}
+                                    </tr>                                    
+                                ))}                                
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-
-
-                
             </div>
         </>
     )
