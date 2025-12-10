11f8883e2dfc495500102774d21e4292bb021327
Fede <fede.delisi@gmail.com>
2025-12-10 21:43:36 +0800

    Add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+function Footer() {
+    return (
+        <footer className="text-sm text-zinc-500 italic space-y-4 mt-24">
+            <p>&copy; 2025 Federica Delisi</p>
+            <p>React · TypeScript · Tailwind CSS</p>
+            <p>
+                <a href="https://github.com/iecya/job-application-tracker" target="_blank" className="underline text-teal-500 hover:text-teal-600">
+                    View source on GitHub
+                </a>
+            </p>
+        </footer>
+    )
+}
+
+
+export default Footer
